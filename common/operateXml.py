181f4f562140cabe702485fba889de3fd561c214
jsj1jsj <441517994@qq.com>
2023-12-14 11:33:20 +0800

    change delete

---
+++ common/operateXml.py
@@ -0,0 +1,40 @@
+from common.utilTool import UtilTool
+class OperateXml(object):
+    def __init__(self,filename=None):
+        if filename:
+            self.filename = filename
+        else:
+            self.filename = UtilTool().get_file_dirname('dataCase/jsonParams','alitrip.xml')
+
+    def get_xml_data(self):
+        with open(self.filename,'r',encoding='utf-8') as fn:
+            data = fn.read()
+            return data
+class OperateXml1(object):
+    def __init__(self,filename=None):
+        if filename:
+            self.filename = filename
+        else:
+            self.filename = UtilTool().get_file_dirname('dataCase/jsonParams','1.xml')
+
+    def get_xml_data(self):
+        with open(self.filename,'r',encoding='utf-8') as fn:
+            data = fn.read()
+            return data
+class OperateXml2(object):
+    def __init__(self,filename=None):
+        if filename:
+            self.filename = filename
+        else:
+            self.filename = UtilTool().get_file_dirname('dataCase/jsonParams','tongchengcancel.xml')
+
+    def get_xml_data(self):
+        with open(self.filename,'r',encoding='utf-8') as fn:
+            data = fn.read()
+            return data
+if __name__ == '__main__':
+    ox = OperateXml()
+    print(ox.filename)
+    data = ox.get_xml_data()
+    print(type(data))
+
